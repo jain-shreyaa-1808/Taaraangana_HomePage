bb0a5f74e6c26bf3a94948db793dc9ec776b8f8a
Yukti Kashyap <yukti102btcsai21@igdtuw.ac.in>
2023-01-27 23:32:26 +0530

    Added register pop-up

--- index.js
+++ index.js
@@ -116,3 +116,17 @@ document.onmousewheel = function(e) {
   radius += d;
   init(1);
 };
+ 
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        2000 
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
