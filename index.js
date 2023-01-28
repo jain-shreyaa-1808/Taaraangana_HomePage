a6d4b78af7321b4858d633bf969ea1c1931673a2
Shreya Jain <shreya061mca22@igdtuw.ac.in>
2023-01-28 13:27:01 +0530

    about us bg width changed

--- index.js
+++ index.js
@@ -116,3 +116,18 @@ document.onmousewheel = function(e) {
   radius += d;
   init(1);
 };
+
+//popup actions
+// window.addEventListener("load", function(){
+//   setTimeout(
+//       function open(event){
+//           document.querySelector(".popup").style.display = "block";
+//       },
+//       2000 
+//   )
+// });
+
+
+// document.querySelector("#close").addEventListener("click", function(){
+//   document.querySelector(".popup").style.display = "none";
+// });
