39ceac5dae53d9298ae88fb6993f8cdef8431777
Shreya Jain <shreya061mca22@igdtuw.ac.in>
2023-01-28 16:26:10 +0530

    Pop-up Added

--- index.js
+++ index.js
@@ -118,16 +118,16 @@ document.onmousewheel = function(e) {
 };
 
 //popup actions
-// window.addEventListener("load", function(){
-//   setTimeout(
-//       function open(event){
-//           document.querySelector(".popup").style.display = "block";
-//       },
-//       2000 
-//   )
-// });
+window.addEventListener("load", function(){
+  setTimeout(
+      function open(event){
+          document.querySelector(".popup").style.display = "block";
+      },
+      2000 
+  )
+});
 
 
-// document.querySelector("#close").addEventListener("click", function(){
-//   document.querySelector(".popup").style.display = "none";
-// });
+document.querySelector("#close").addEventListener("click", function(){
+  document.querySelector(".popup").style.display = "none";
+});
